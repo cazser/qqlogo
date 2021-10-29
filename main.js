d59a651a30c1b5364f4c544b5b1959e0cf9f368b
cazser <2264712072@qq.com>
2021-10-29 13:40:46 +0800

    播放进程5

--- main.js
+++ main.js
@@ -4,13 +4,20 @@ let bar = document.querySelector(".bar")
 
 
 let link = document.querySelector("link")
-if (link) {
-    link.remove();
+
+function doPlay() {
+    if (link) {
+        link.remove();
+    }
+
+
+    for (let i = 1; i <= 20; i++) {
+        let part = document.createElement("link");
+        part.rel = "stylesheet"
+        part.href = `./css/step${i}.css`
+        document.head.appendChild(part)
+    }
 }
 
 
-for (let i = 1; i <= 20; i++) {
-    let part = document.createElement("link");
-    part.rel = "stylesheet"
-    part.href = `./css/step${i}.css`
-}
+play.addEventListener("click", doPlay);
