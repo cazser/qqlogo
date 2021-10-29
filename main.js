5f244c9deb37fee2edf0b354e142c6b43ca8f4a3
cazser <2264712072@qq.com>
2021-10-29 13:34:49 +0800

    播放进程3

--- main.js
+++ main.js
@@ -10,5 +10,7 @@ if (link) {
 
 
 for (let i = 1; i <= 20; i++) {
-    console.log(i);
+    let part = document.createElement("link");
+    part.rel = "stylesheet"
+    part.href = `./css/step${i}.css`
 }
