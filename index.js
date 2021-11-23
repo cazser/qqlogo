5933b61d8b2a1d17082f7b0d5ad28f5a9a707e7b
cazser <2264712072@qq.com>
2021-11-23 14:40:11 +0800

    修改2

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const playButton = document.querySelector('.play')
 const code = document.querySelector('.code')
 const paddingCode = document.querySelector('.paddingCode')
+playButton.addEventListener("click", () => {
+    let link = document.querySelector("a")
+})
